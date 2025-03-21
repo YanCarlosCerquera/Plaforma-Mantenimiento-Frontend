a6019e1107da3a13bc3109490aebfb4f43080450
jhonnCorredor <jhoncorredormedina29@gmail.com>
2025-03-21 11:44:57 -0500

    Cambio ruta back

--- src/service/apiservice.js
+++ src/service/apiservice.js
@@ -4,7 +4,7 @@ import Cookies from "js-cookie";
 const jwt_decode = require("jwt-decode");
 
 // Acceder a la variable de entorno de Vue
-const API_BASE_URL = process.env.VUE_APP_API_URL || "https://king-prawn-app-2u9tk.ondigitalocean.app";
+const API_BASE_URL = process.env.VUE_APP_API_URL || "https://stingray-app-e496q.ondigitalocean.app";
 
 // Crear cliente API
 const apiClient = axios.create({
