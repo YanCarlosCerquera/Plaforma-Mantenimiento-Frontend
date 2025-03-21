4b945b0b15e2f6df1ae74d752b223c63b194adac
jhonnCorredor <jhoncorredormedina29@gmail.com>
2025-03-21 11:32:33 -0500

    Arreglos

--- src/examples/Navbars/Navbar.vue
+++ src/examples/Navbars/Navbar.vue
@@ -1,15 +1,13 @@
 <script setup>
-import { computed,  } from "vue";
+import { computed } from "vue";
 import { useStore } from "vuex";
 
 const store = useStore();
 const isRTL = computed(() => store.state.isRTL);
 
-
-
 const minimizeSidebar = () => store.commit("sidebarMinimize");
-
 </script>
+
 <template>
   <nav
     class="navbar navbar-main navbar-expand-lg px-0 mx-4 shadow-none border-radius-xl"
@@ -19,8 +17,6 @@ const minimizeSidebar = () => store.commit("sidebarMinimize");
     data-scroll="true"
   >
     <div class="px-3 py-1 container-fluid">
-      
-
       <div
         class="mt-2 collapse navbar-collapse mt-sm-0 me-md-0 me-sm-4"
         :class="isRTL ? 'px-0' : 'me-sm-4'"
@@ -30,10 +26,9 @@ const minimizeSidebar = () => store.commit("sidebarMinimize");
           class="pe-md-3 d-flex align-items-center"
           :class="isRTL ? 'me-md-auto' : 'ms-md-auto'"
         >
-         
+          <!-- Otros elementos del navbar -->
         </div>
         <ul class="navbar-nav justify-content-end">
-        
           <li class="nav-item d-xl-none ps-3 d-flex align-items-center">
             <a
               href="#"
@@ -49,13 +44,22 @@ const minimizeSidebar = () => store.commit("sidebarMinimize");
             </a>
           </li>
           <li
-            class="nav-item dropdown mx-3 d-flex align-items-center"
+            class="nav-item dropdown mx-3 d-flex align-items-center d-none d-md-block"
             :class="isRTL ? 'ps-2' : 'pe-2'"
-            >
-            <img src="../../assets/logoSena.jpg" alt="SENA Logo" class="sena-logo" /> 
+          >
+            <img src="../../assets/logoSena.jpg" alt="SENA Logo" class="sena-logo" />
           </li>
         </ul>
       </div>
     </div>
   </nav>
 </template>
+
+<style scoped>
+/* Ocultar el logo en dispositivos móviles */
+@media (max-width: 767.98px) {
+  .sena-logo {
+    display: none;
+  }
+}
+</style>
